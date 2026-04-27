b722fcc8814b3f624d73406f96dfeda02311de6e
kunal7800 <kunal1361.becse24@chitkara.edu.in>
2026-04-27 18:39:57 +0530

    fixed error

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const app = require('./src/app');
 
+console.log("GROQ_API:", process.env.GROQ_API);
+
 app.listen(5000, () => {
     console.log('Server Started at port 5000');
 

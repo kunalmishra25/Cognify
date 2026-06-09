509b3ea1d09ba000570e0465dac7528806d7a698
kunal7800 <kunal1361.becse24@chitkara.edu.in>
2026-06-10 00:02:55 +0530

    Created summary Schema

---
+++ backend/src/models/summary.model.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose")
+
+
+const summarySchema = new mongoose.Schema({
+
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "user",
+        required: true
+    },
+    fileName: {
+        type: String,
+        required: true
+    },
+    summary: {
+        type: String
+    },
+    createdAt: {
+        type: String,
+        required: true
+    }
+})
+
+const summaryModel = mongoose.model("summary", summarySchema)
+module.exports = summaryModel
